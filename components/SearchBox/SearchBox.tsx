25178ef67c5f2e90e1636f58cd4d0b6c5b922042
Margarita Gladkowa <margarita198700@gmail.com>
2025-10-27 16:50:58 +0300

    auto commit

--- components/SearchBox/SearchBox.tsx
+++ components/SearchBox/SearchBox.tsx
@@ -1,25 +1,19 @@
 import React from "react";
 import css from "./SearchBox.module.css";
 
-// 1. Змінено назву інтерфейсу на SearchBoxProps
 interface SearchBoxProps {
-  // 2. Пропс value зроблено опціональним (?)
   value?: string;
-  // Колбек onSearch залишається обов'язковим
+
   onSearch: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-// Використовуємо новий інтерфейс
 export default function SearchBox({ value = "", onSearch }: SearchBoxProps) {
-  // Встановлюємо значення за замовчуванням ("") для value у деструктуризації,
-  // якщо воно опціональне і не передане, щоб уникнути помилок.
-
   return (
     <input
       className={css.input}
       type="text"
       placeholder="Search notes"
-      onChange={(e) => onSearch(e.target.value)}
+      onChange={onSearch}
       value={value}
       aria-label="Search notes"
     />
